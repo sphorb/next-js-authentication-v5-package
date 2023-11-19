1c6cdc4ba3554cf8fb7b009c44e1c8867e12901d
maghfoor-dev <maghfoor.dev@gmail.com>
2023-11-19 19:31:58 +0000

    added links to go to dashboard and back

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function DashBoard() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>DASHBOARD PAGE</h1>
+      <Link href={"/"} className="bg-[#F1C617] text-black">
+        GO TO HOME
+      </Link>
     </main>
   );
 }
